10cb4fddc0e46c8b37c085a0fbaf0038767f2f26
BhanuPrasad-Boda <sivabhanuboda@gmail.com>
2025-11-16 19:02:22 +0530

    updated

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 function Typewriter() {
   const fullText = "Hello Everyone! I am Bhanuprasad Boda Passionate Frontend Developer.";
   const [displayedText, setDisplayedText] = useState('');
-  const typingSpeed = 100; // milliseconds per character
+  const typingSpeed = 100;
 
   useEffect(() => {
     if (displayedText.length < fullText.length) {
@@ -14,74 +14,58 @@ function Typewriter() {
       }, typingSpeed);
       return () => clearTimeout(timer);
     }
-  }, [displayedText, fullText, typingSpeed]);
+  }, [displayedText, fullText]);
 
   return (
     <div className='d-flex justify-content-center m-4'>
-      <h3><i>{displayedText}</i></h3>
+      <h3>
+        <i>{displayedText}</i>
+        <span className="cursor">|</span>
+      </h3>
     </div>
   );
 }
 
 export default function Home() {
-
   const [sample, setSample] = useState('bi bi-sun');
   const [mode, setMode] = useState('');
 
-  function handleclick(e){
+  function handleclick() {
     if(sample==='bi bi-sun'){
       setSample('bi bi-moon-fill')
       setMode('dark-mode')
-    }else{
+    } else {
       setSample('bi bi-sun')
       setMode('')
     }
   }
 
+  return (
+    <div className={`home ${mode}`}>
+      {/* Dark mode toggle */}
+      <div className={`toggle-icon ${sample}`} onClick={handleclick}></div>
 
+      <div className="hero-section container-fluid d-flex justify-content-center flex-wrap">
+        {/* IMAGE */}
+        <div className="section-image w-100 d-flex justify-content-center align-items-center">
+          <img className="rounded-circle" src="./images/Img.png" alt="Profile" />
+        </div>
 
- return (
-  <div className={`home ${mode} `}>
-    <div className={` ${sample} p-2`} onClick={handleclick}></div>
+        {/* TYPEWRITER */}
+        <div className="section-typewriter w-100 d-flex justify-content-center">
+          <Typewriter />
+        </div>
 
-    <div className="w-100 container-fluid d-flex justify-content-between flex-wrap">
-
-      {/* IMAGE */}
-      <div className="w-100 d-flex img justify-content-center align-items-center section-image">
-        <img className="rounded-circle" width={200} src="./images/Img.png" alt="Description" />
-      </div>
-
-      {/* TYPEWRITER */}
-      <div className="section-typewriter w-100 d-flex justify-content-center">
-        <Typewriter />
-      </div>
-
-      {/* BUTTONS */}
-      <div className="w-100 d-flex justify-content-center align-items-center section-buttons">
-        <div className="container w-50">
-        
-          <div className="row r1 d-flex justify-content-center align-items-end">
-            <Link to='/about' className='ab1'>
-              <div className="col-6 d-flex justify-content-center">About</div>
-            </Link>
-            <Link to='/skills' className='ab1'>
-              <div className="col-6 d-flex justify-content-center">Skills</div>
-            </Link>
+        {/* BUTTONS */}
+        <div className="section-buttons w-100 d-flex justify-content-center align-items-center">
+          <div className="button-container container w-50">
+            <Link to='/about'><div className="col-6">About</div></Link>
+            <Link to='/skills'><div className="col-6">Skills</div></Link>
+            <Link to='/projects'><div className="col-6">Projects</div></Link>
+            <Link to='/contact'><div className="col-6">Contact</div></Link>
           </div>
-
-          <div className="row r2">
-            <Link to='/projects' className='ab'>
-              <div className="col-6 d-flex justify-content-center">Projects</div>
-            </Link>
-            <Link to='/contact' className='ab'>
-              <div className="col-6 d-flex justify-content-center">Contact</div>
-            </Link>
-          </div>
-
         </div>
       </div>
-
     </div>
-  </div>
-)
+  )
 }
